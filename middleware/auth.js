13ff441cd09b226734015786fd053bf4613fe70c
Ephy Kimotho <ephyk48@gmail.com>
2025-06-16 11:00:31 +0300

    chore: created a middleware to getUser and authenticate ppassword on login

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,8 @@
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
-const { CustomError } = require("../utils/index");
+const bcrypt = require("bcryptjs");
+const Users = require("../models/users");
+const { AuthError, CustomError } = require("../utils/index");
 
 // Load the environment variables
 dotenv.config();
@@ -15,7 +17,7 @@ function authenticate(req, res, next) {
   accessToken = accessToken.replace(/^bearer\s*/i, "");
 
   if (!accessToken) {
-    return next(new CustomError("No access token provided.", 401));
+    return next(new AuthError("No access token provided.", 401));
   }
 
   // Verify token signature
@@ -37,6 +39,39 @@ function authenticate(req, res, next) {
   }
 }
 
+async function getUser(req, res, next) {
+  // Get the user fields from req object
+  const fields = req.body;
+
+  // Get the field (email/phone) used to query db
+  const filter = fields.email
+    ? { email: fields.email }
+    : { phone: fields.phone };
+
+  // Get the user object from DB
+  const user = await Users.get(filter);
+
+  // if no user is found throw not found error
+  if (!user) {
+    throw next(new CustomError("User not found!", 404));
+  }
+
+  // check if password match
+  const passwordMatch = await bcrypt.compare(fields.password, user.password);
+
+  if (!passwordMatch) {
+    // if not throw error
+    throw next(new AuthError("Incorrect password!", 400));
+  }
+
+  // if they match attach user object to req
+  req.user = user;
+
+  // call next
+  next();
+}
+
 module.exports = {
   authenticate,
+  getUser,
 };
