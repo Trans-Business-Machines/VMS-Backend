a833ac25c741d60373e7b6f2ff27e9687c8a7f15
Ephy Kimotho <ephyk48@gmail.com>
2025-06-17 12:10:41 +0300

    chore: added the logout, refresh-token and new visit routes

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,17 @@ const express = require("express");
 const router = express.Router();
 
 // Internal module methods
-const { register, login } = require("../controllers/auth");
+const {
+  register,
+  login,
+  logout,
+  refreshTokens,
+} = require("../controllers/auth");
 const { getUser } = require("../middleware/auth");
 
 router.post("/register", register);
 router.post("/login", getUser, login);
+router.post("/refresh-token", refreshTokens);
+router.post("/logout", logout);
 
 module.exports = router;
