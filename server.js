b9b1d88269a9f30160b747a101072f9a415e0cf2
Ephy Kimotho <ephyk48@gmail.com>
2025-06-18 15:01:56 +0300

    chore: hooked the userRoutes into the app router

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cookieParser = require("cookie-parser");
 // Internal modules
 const authRoutes = require("./routes/auth");
 const visitRoutes = require("./routes/visits");
+const userRoutes = require("./routes/users");
 const middleware = require("./middleware/index");
 
 // Load the environment variables
@@ -25,6 +26,7 @@ app.use(cookieParser()); // parse cookies
 // App routes
 app.use("/api/auth", authRoutes);
 app.use("/api/visit", visitRoutes);
+app.use("/api/users", userRoutes);
 
 // Error handling middleware and NotFound middleware
 app.use(middleware.handleError);
