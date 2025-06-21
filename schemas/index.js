0cc4e05db99c939302500681348f646e4489c4d7
Ephy Kimotho <ephyk48@gmail.com>
2025-06-21 12:16:06 +0300

    chore: created the visit schema

--- schemas/index.js
+++ schemas/index.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 const { createId } = require("@paralleldrive/cuid2");
 const { isAlpha, isEmail, isNumeric } = require("validator");
 
+// Import internal modules
+const { VisitPurpose, visitPurposes } = require("../utils");
+
 const userSchema = new mongoose.Schema(
   {
     _id: { type: String, default: createId },
@@ -57,6 +60,86 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+/* ----------------- visit schema ----------------- */
+const visitSchema = new mongoose.Schema({
+  _id: { type: String, default: createId },
+  host: {
+    type: String,
+    ref: "Users",
+    index: true,
+    required: true,
+  },
+  firstname: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 30,
+    validate: {
+      validator: isAlpha,
+      message: (props) => `${props.value} should only contain alphabets`,
+    },
+  },
+  lastname: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 30,
+    validate: {
+      validator: isAlpha,
+      message: (props) => `${props.value} should only contain alphabets`,
+    },
+  },
+  national_id: {
+    type: String,
+    required: true,
+    index: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    maxLength: 15,
+    minLength: 10,
+    validate: {
+      validator: isNumeric,
+      message: (props) => `${props.value} should only contain numbers`,
+    },
+  },
+  status: {
+    type: String,
+    required: true,
+    lowercase: true,
+    enum: ["checked-in", "checked-out"],
+    default: "checked-in",
+  },
+  purpose: {
+    type: String,
+    required: true,
+    lowercase: true,
+    enum: visitPurposes,
+    default: VisitPurpose.BUSINESS_MEETING,
+  },
+  visit_date: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  time_in: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  time_out: {
+    type: Date,
+  },
+  checkin_officer: {
+    type: String,
+    ref: "Users",
+    index: true,
+    required: true,
+  },
+});
+
 module.exports = {
   userSchema,
+  visitSchema,
 };
