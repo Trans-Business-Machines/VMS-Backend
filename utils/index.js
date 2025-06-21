5a64642a2168e7f087bb35d1e0d0086e5790cb3d
Ephy Kimotho <ephyk48@gmail.com>
2025-06-21 12:18:15 +0300

    chore: added the visitPurpose object containing possible reasons of visit to an org

--- utils/index.js
+++ utils/index.js
@@ -72,4 +72,48 @@ async function sendEmail(user, password) {
   }
 }
 
-module.exports = { AuthError, CustomError, sendEmail };
+// This object defines the various purposes for which a visit can be made.
+// Add other purposes as needed.
+// Each purpose is represented as a key-value pair, where the key is a constant
+const VisitPurpose = {
+  BUSINESS_MEETING: "business meeting",
+  JOB_INTERVIEW: "job interview",
+  CLIENT_CONSULTATION: "client consultation",
+  VENDOR_DELIVERY_COURIER: "vendor delivery",
+  MAINTENANCE_REPAIR: "maintenance",
+  IT_SUPPORT: "it support",
+  TRAINING_WORKSHOP: "training workshop",
+  OFFICE_TOUR: "office tour",
+  INSPECTION_AUDIT: "inspection",
+  EXECUTIVE_VISIT: "executive visit",
+  NETWORKING_EVENT: "networking event",
+  HR_APPOINTMENT: "hr appointment",
+  LEGAL_COMPLIANCE_MEETING: "legal compliance meeting",
+  FOLLOW_UP: "follow up",
+};
+
+// This array contains all the visit purposes defined in the VisitPurpose object.
+const visitPurposes = [
+  VisitPurpose.BUSINESS_MEETING,
+  VisitPurpose.JOB_INTERVIEW,
+  VisitPurpose.CLIENT_CONSULTATION,
+  VisitPurpose.VENDOR_DELIVERY_COURIER,
+  VisitPurpose.MAINTENANCE_REPAIR,
+  VisitPurpose.IT_SUPPORT,
+  VisitPurpose.TRAINING_WORKSHOP,
+  VisitPurpose.OFFICE_TOUR,
+  VisitPurpose.INSPECTION_AUDIT,
+  VisitPurpose.EXECUTIVE_VISIT,
+  VisitPurpose.NETWORKING_EVENT,
+  VisitPurpose.HR_APPOINTMENT,
+  VisitPurpose.LEGAL_COMPLIANCE_MEETING,
+  VisitPurpose.FOLLOW_UP,
+];
+
+module.exports = {
+  AuthError,
+  CustomError,
+  sendEmail,
+  VisitPurpose,
+  visitPurposes,
+};
