00319caf00f81b62e6a2409a8cc57f9fe954d9a8
Ephy Kimotho <ephyk48@gmail.com>
2025-07-05 09:53:40 +0300

    util: created a utility function to check if host is available on check-in

---
+++ utils/services.js
@@ -0,0 +1,40 @@
+const { isAfter, isBefore, format } = require("date-fns");
+const { CustomError } = require("./");
+
+async function isHostAvailable(visitorData, currentDate, getHostAvailabilty) {
+  const schedule = await getHostAvailabilty(visitorData.host);
+
+  if (!schedule) {
+    throw new CustomError(
+      "This host has no schedule yet. Please contact the reception or select another host.",
+      400
+    );
+  }
+
+  const start = new Date(schedule.start_date);
+  const end = new Date(schedule.end_date);
+
+  if (isBefore(currentDate, start)) {
+    throw new CustomError(
+      `The host is not yet available. Their availability starts on ${format(
+        start,
+        "PPP"
+      )}. Please choose another host.`,
+      400
+    );
+  }
+
+  if (isAfter(currentDate, end)) {
+    throw new CustomError(
+      `The host's availability ended on ${format(
+        end,
+        "PPP"
+      )}. Please choose another host.`,
+      400
+    );
+  }
+
+  return true;
+}
+
+module.exports = { isHostAvailable };
